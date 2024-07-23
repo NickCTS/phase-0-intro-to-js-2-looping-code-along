4ec909ef6db16149965de1265b639263acde7886
nicholas <nickcts@hotmail.com>
2024-07-23 19:35:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Code your solutions in this file 
+function writeCards(Names) {
+    let thankYouMessages = []; // Use a different name for the array
+  
+    for (let i = 0; i < Names.length; i++) {
+      let message = `Thank you, ${Names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message); // Add the message to the array
+    }
+  
+    return thankYouMessages;
+  }
+  
+  let Names = ["Guadalupe", "Ollie", "Aki"];
+  console.log(writeCards(Names));
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+}
+countDown(10);
